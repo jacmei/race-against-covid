5a852d99f7fb988d0e80ca7b346c8e4338f9c904
Joey Leung <jleung147@gmail.com>
2020-04-30 16:24:40 -0400

    Re-align text

--- public/benchmark2/js/scenes/LevelOneScene.js
+++ public/benchmark2/js/scenes/LevelOneScene.js
@@ -12,8 +12,8 @@ class LevelOneScene extends LevelScene {
         super.loadTimer(this.timeLeft);
         super.loadHP();
         //display instructions for hp, and shooting
-        this.add.text(this.rooms[0].x+200,
-            this.rooms[0].y+350,
+        this.add.text(this.rooms[0].x+165,
+            this.rooms[0].y+450,
             'Click to shoot pills that can damage the viruses. Be careful not to shoot \n'+
             'recklessly, as every shot will reduce RX-2020\'s hp. You must reach \n'+
             'the stomach with at least 20 hp or else you won\'t be effective enough \n'+
@@ -24,11 +24,20 @@ class LevelOneScene extends LevelScene {
         //display instructions for hp, and shooting
         this.add.text(this.rooms[2].x+200,
             this.rooms[2].y+350,
-            'After picking up an upgrade, press q or e to upgrade \n'+
-            'your weapon or health before continuing the game. \n'+
-            'You will not be able to move until after you \n'+
-            'upgrade, and your weapon can only be upgraded 2 times',
+            'Orange slices will heal you for 20 HP!',
             {color: 'white', font: '15px'});
+
+            
+        //display instructions for hp, and shooting
+        this.add.text(this.rooms[1].x+100,
+            this.rooms[1].y+100,
+            'Tap or hold left click to fire your medicine gun.\n'+
+            'After gathering enough points, you will be able to\n'+
+            'upgrade either your health or your weapon!\n'+
+            'Your weapon may only be upgraded twice.',
+            {color: 'white', font: '15px'});
+        
+            this.add
         
         
     }
