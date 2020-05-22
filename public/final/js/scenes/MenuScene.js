fe56053b7489dbcd39f9039f6b2e00e6d5ef3530
Joey Leung <jleung147@gmail.com>
2020-05-21 23:11:39 -0400

    Audio Mute etc

--- public/final/js/scenes/MenuScene.js
+++ public/final/js/scenes/MenuScene.js
@@ -56,5 +56,26 @@ class MenuScene extends Phaser.Scene {
             this.scene.start(ABOUT);
         })
 
+
+
+
+        
+        var audioButton;
+        if(this.sound.sounds[0].mute){
+            audioButton = this.add.image(50,585, "mutebutton");
+        }else{
+            audioButton = this.add.image(50,585, "unmutebutton");
+        }
+        audioButton.setScale(.1);
+        audioButton.setInteractive();
+        audioButton.on("pointerup", () => {
+            if(this.sound.sounds[0].mute){
+                this.sound.sounds[0].setMute(false);
+                audioButton.setTexture("unmutebutton");
+            }else{
+                this.sound.sounds[0].setMute(true);
+                audioButton.setTexture("mutebutton");
+            }
+        });
     }
 }
