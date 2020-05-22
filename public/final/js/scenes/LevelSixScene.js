343a67ac4de28d6a2965ca6ff238ffe3f5a9dbfb
Andy Cen <cen.andy.2016@gmail.com>
2020-05-22 04:17:05 -0400

    Fixed boss spawn

--- public/final/js/scenes/LevelSixScene.js
+++ public/final/js/scenes/LevelSixScene.js
@@ -20,7 +20,7 @@ class LevelSixScene extends LevelScene {
         // all viruses in last room killed
         if (this.virusCount[this.rooms.length-1] == 0 && !this.bossSpawned) {
             this.bossSpawned = true;
-            let boss = new BossVirus(this, this.rooms[0].x+WIDTH/2-100, this.rooms[0].y+100, BOSS);
+            let boss = new BossVirus(this, this.rooms[this.rooms.length-1].x+WIDTH/2-100, this.rooms[this.rooms.length-1].y+100, BOSS);
             boss.canMove = true;
             this.addVirus(boss);
         }
